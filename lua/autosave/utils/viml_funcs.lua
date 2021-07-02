89f29f9774bc5b1d69b7a9da856c864af06b4365
kdav5758 <kdav5758@gmail.com>
2021-07-01 23:56:33 -0500

    fixed bufdo: restore previous view as well

--- lua/autosave/utils/viml_funcs.lua
+++ lua/autosave/utils/viml_funcs.lua
@@ -5,9 +5,16 @@ local api = vim.api
 api.nvim_exec(
     [[
 	function! g:AutoSaveBufDo(command)
+
+		let b:autosave_view_before_bufdo = winsaveview()
+
 		let currBuff=bufnr("%")
 		execute 'bufdo ' . a:command
 		execute 'buffer ' . currBuff
+
+		call winrestview(b:autosave_view_before_bufdo)
+		unlet b:autosave_view_before_bufdo
+
 	endfunction
 	com! -nargs=+ -complete=command Bufdo call BufDo(<q-args>)
 ]],
