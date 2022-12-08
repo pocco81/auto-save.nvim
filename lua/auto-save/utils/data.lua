7f6454795e4eb64a26c9a23e963da4988cd2e4b5
singlexyz <singlexyz@gmail.com>
2022-12-09 04:20:56 +0800

    fix callbacks never call

--- lua/auto-save/utils/data.lua
+++ lua/auto-save/utils/data.lua
@@ -3,23 +3,17 @@ local M = {}
 local cnf = require("auto-save.config").opts
 
 function M.set_of(list)
-	local set = {}
-	for i = 1, #list do
-		set[list[i]] = true
-	end
-	return set
+  local set = {}
+  for i = 1, #list do
+    set[list[i]] = true
+  end
+  return set
 end
 
 function M.not_in(var, arr)
-	if M.set_of(arr)[var] == nil then
-		return true
-	end
-end
-
-function M.do_callback(callback_name)
-	if type(cnf.callbacks[callback_name]) == "function" then
-		cnf.callbacks[callback_name]()
-	end
+  if M.set_of(arr)[var] == nil then
+    return true
+  end
 end
 
 return M
